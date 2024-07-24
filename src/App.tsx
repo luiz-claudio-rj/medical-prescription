87ead4f9ebb95b2594eeb84c0ad35ae99ef5bec0
Luiz Claudio <lcluizclaudio96@gmail.com>
2024-07-24 03:09:08 -0300

    feat: config theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-export default function App() {
+import React from "react";
+import PrescriptionForm from "./PrescriptionForm";
+import { ThemeProvider, useTheme } from "./context/theme";
+import { Button } from "./components/ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+const AppContent: React.FC = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div className={`App ${theme} relative`}>
+      <header className="sticky top-0  bg-slate-200 dark:bg-slate-800 z-10">
+        <div className="container flex gap-4 pt-4 justify-between  sm:px-24 items-center">
+          <h1 className="text-xl sm:text-2xl font-bold mb-4 ">
+            Gerador de prescrição médica
+          </h1>
+          <Button onClick={toggleTheme} className="mb-4 bg-transparent">
+            {theme === "light" ? (
+              <MoonIcon className="h-8 w-8" color="black" />
+            ) : (
+              <SunIcon className="h-8 w-8" color="white" />
+            )}
+          </Button>
+        </div>
+      </header>
+      <div className="container relative">
+        <PrescriptionForm />
+      </div>
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/context/theme.tsx
@@ -0,0 +1,58 @@
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextProps {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const updateTheme = (theme: Theme) => {
+    localStorage.setItem("theme", theme);
+    setTheme(theme);
+  };
+
+  const toggleTheme = () => {
+    updateTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme") as Theme;
+    if (localTheme) {
+      setTheme(localTheme);
+      return;
+    }
+
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    setTheme(prefersDarkMode ? "dark" : "light");
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
